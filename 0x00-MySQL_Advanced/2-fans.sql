1ae4b0e0735ac4ec4a4be97b58b5a3926977e061
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-12 23:35:02 +0300

    fans column rename

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Calculate the total number of non-unique fans for each country origin
-SELECT origin, SUM(nb_fans) as nb_fans
+SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
