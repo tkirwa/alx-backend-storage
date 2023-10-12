282a18dd84354630aafba506f3757eb3e6cf4f6b
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-13 00:28:25 +0300

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,23 +1,13 @@
 -- Create a stored procedure to compute and store the average score for a user
 DELIMITER //
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
-    DECLARE total_score INT;
-    DECLARE number_of_corrections INT;
-    DECLARE average FLOAT;
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
 
-    -- Calculate the total score and number of corrections for the user
-    SELECT SUM(score), COUNT(*) INTO total_score, number_of_corrections
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Calculate the average score (use COALESCE to handle division by zero)
-    SET average = COALESCE(total_score / number_of_corrections, 0);
-
-    -- Update the user's average score
-    UPDATE users
-    SET average_score = average
-    WHERE id = user_id;
 END;
 //
 DELIMITER ;
