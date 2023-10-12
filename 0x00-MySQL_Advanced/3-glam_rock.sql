108faa953eea4cef444271cbf40a8ecf04d66425
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-12 23:44:09 +0300

    fix null field-column

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- Lists all bands with Glam rock as their main style - ranked by their longevity
 
 SELECT band_name, 
-       IF(split = 0, 2022 - formed, split - formed) as lifespan
+       IFNULL(NULLIF(IF(split = 0, 2022 - formed, split - formed), -formed), 0) as lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
