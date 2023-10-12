214b825ef997f4477421c86737bc017f3949b19f
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-12 23:46:47 +0300

    fix - column

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,9 @@
 -- Calculate the lifespan of bands with the main style "Glam rock" until 2022
 -- Lists all bands with Glam rock as their main style - ranked by their longevity
 
-SELECT band_name, 
-       IFNULL(NULLIF(IF(split = 0, 2022 - formed, split - formed), -formed), 0) as lifespan
+SELECT band_name,
+        (IFNULL(split, 2022) - formed)
+AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
