987b2eaa2fddc5686086b2944306ce4e875aad34
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-12 23:31:39 +0300

    metal_bands --fix

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Calculate the total number of non-unique fans for each country origin
 SELECT origin, SUM(nb_fans) as nb_fans
-FROM bands
+FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
