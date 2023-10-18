35d14b764342fa86bc87a787e325576d9980359c
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-18 20:24:20 +0300

    web.py - implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,50 +1,32 @@
 #!/usr/bin/env python3
-"""Web Cache and Tracker"""
+""" Redis Module Cache and tracking """
 
+from functools import wraps
 import redis
 import requests
-from functools import wraps
 from typing import Callable
 
-# Initialize a Redis client
-redis_client = redis.Redis()
+redis_ = redis.Redis()
 
 
 def count_requests(method: Callable) -> Callable:
-    """Decorator to count and cache requests"""
-
+    """ Decortator for counting """
     @wraps(method)
-    def wrapper(url: str) -> str:
-        """Wrapper function for counting and caching requests"""
-        # Increment the count for the URL
-        redis_client.incr(f"count:{url}")
-
-        # Check if the response is cached
-        cached_response = redis_client.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode("utf-8")
-
-        # If not cached, make a request
-        response = method(url)
-
-        # Cache the response with a 10-second expiration time
-        redis_client.setex(f"cached:{url}", 10, response)
-
-        return response
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
 
     return wrapper
 
 
 @count_requests
 def get_page(url: str) -> str:
-    """Fetch the HTML content of a URL"""
-    response = requests.get(url)
-    return response.text
-
-
-if __name__ == "__main__":
-    # Example usage of the get_page function
-    url = "http://slowwly.robertomurray.co.uk"
-    content = get_page(url)
-    print(f"Content of {url} (cached):")
-    print(content)
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
