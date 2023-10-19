44349e494d7897ffd369e9bcb1dba0e545bc5519
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-19 10:13:36 +0300

    web.py - implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python3
 """Web Cache and Tracker using Redis.
 
-This module defines a function to fetch and cache web pages while tracking access counts.
-
-Example:
-    To use the get_page function:
-    >>> content = get_page("http://example.com")
-    >>> print(content)
-
-Requirements:
-    - Python 3.7
-    - pycodestyle 2.5
-    - redis-py library
+This module defines a function to fetch and cache web pages while tracking
+ access counts.
 """
 
 import redis
@@ -31,7 +22,8 @@ def get_page(url: str) -> str:
         str: The HTML content of the URL.
 
     Note:
-        This function caches the content in Redis with an expiration time of 10 seconds.
+        This function caches the content in Redis with an expiration time
+          of 10 seconds.
         It also tracks the number of times the URL was accessed.
 
     Example:
