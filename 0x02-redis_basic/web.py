8ba295477655d5decfc0ac4c6db8398bd5fb80fa
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-19 23:37:58 +0300

    get_page update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -11,30 +11,12 @@ r = redis.Redis()
 
 
 def count_calls(method: Callable) -> Callable:
-    """
-    Decorator that counts calls made to the method.
-
-    Args:
-        method (Callable): The method for which to count calls.
-
-    Returns:
-        Callable: The decorated method.
-    """
-    # Get the qualified name of the method
+    """Decorator that counts calls made to the method."""
     key = method.__qualname__
 
     @wraps(method)
     def wrapper(url):
-        """
-        Wrapper function for decorator functionality.
-
-        Args:
-            url (str): The URL to get the page from.
-
-        Returns:
-            str: The HTML content of the page.
-        """
-        # Increment the count for this method in Redis
+        """Wrapper function for decorator functionality."""
         r.incr(key)
         return method(url)
 
@@ -43,20 +25,8 @@ def count_calls(method: Callable) -> Callable:
 
 @count_calls
 def get_page(url: str) -> str:
-    """
-    Get the HTML content of a particular URL and cache it.
-
-    Args:
-        url (str): The URL to get the page from.
-
-    Returns:
-        str: The HTML content of the page.
-    """
-    # Send a GET request to the URL
-    resp = requests.get(f"http://slowwly.robertomurray.co.uk/"
-                        "delay/3000/url/{url}")
-
-    # Get the text of the response
+    """Get the HTML content of a particular URL and cache it."""
+    resp = requests.get(url)
     page = resp.text
 
     # Cache the result with an expiration time of 10 seconds
