d6f5705fae3749ce3ad11518d3873a7bf53a12bd
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-19 10:41:38 +0300

    web.py - implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python3
-"""
-web cache and tracker
-"""
-import requests
+""" expiring web cache module """
+
 import redis
+import requests
+from typing import Callable
 from functools import wraps
 
-store = redis.Redis()
+redis = redis.Redis()
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
     def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
 
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
     return wrapper
 
 
-@count_url_access
+@wrap_requests
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """get page self descriptive
+    """
+    response = requests.get(url)
+    return response.text
