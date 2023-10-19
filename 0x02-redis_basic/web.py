a8d3958fa0e50cf604d907dd4756bf24c04f5697
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-10-20 00:05:23 +0300

    web.py - implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,36 @@
 #!/usr/bin/env python3
 """
-Create a web cache
+web cache and tracker
 """
-import redis
 import requests
+import redis
+from functools import wraps
 
-# Create a connection to the Redis server
-rc = redis.Redis()
+store = redis.Redis()
 
 
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
+
+
+@count_url_access
 def get_page(url: str) -> str:
-    """
-    Get a page and cache value.
-    Args:
-        url (str): The URL to get the page from.
-    Returns:
-        str: The HTML content of the page.
-    """
-    # Check if the page is already in cache
-    page = rc.get(url)
-
-    # If the page is not in cache, send a GET request to the URL
-    if not page:
-        resp = requests.get(url)
-        page = resp.text
-
-        # Cache the result with an expiration time of 10 seconds
-        rc.setex(url, 10, page)
-
-        # Increment the count for this URL
-        rc.incr(f"count:{url}")
-
-    return page
-
-
-if __name__ == "__main__":
-    get_page("http://slowwly.robertomurray.co.uk")
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
